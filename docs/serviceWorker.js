1273d9e85e4f039d548ea2832d01716cac47d448
Oded Sharon <odedshr@gmail.com>
2023-07-29 21:43:39 +0100

    GM-03: added notification

--- docs/serviceWorker.js
+++ docs/serviceWorker.js
@@ -1,3 +1,13 @@
 self.addEventListener("install", installEvent => {
   alert("Service worker installed");
 });
+
+Notification.requestPermission().then(result => {
+  if (result) {
+    const img = "/olaf.jpg";
+    const text = `notification 1`;
+    const notification = new Notification("To do list", { body: text, icon: img });
+  } else {
+    alert("No permission 1");
+  }
+});
